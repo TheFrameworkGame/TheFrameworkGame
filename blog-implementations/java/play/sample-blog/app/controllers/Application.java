5f76512ef46cd2b5032adda668dc8d553452bce3
Richard Warburton <richard.warburton@gmail.com>
2012-01-22 14:34:20 +0000

    adds authentication

--- blog-implementations/java/play/sample-blog/app/controllers/Application.java
+++ blog-implementations/java/play/sample-blog/app/controllers/Application.java
@@ -3,11 +3,21 @@ package controllers;
 import java.util.List;
 
 import models.Post;
+import models.User;
 import play.data.validation.Required;
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Application extends Controller {
 
+    @Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("user", user.fullname);
+        }
+    }
+	
 	public static void addCommentToPost(@Required final Long postId,
 			@Required final String author, @Required final String content,
 			@Required final String email) {
--- blog-implementations/java/play/sample-blog/app/controllers/Categories.java
+++ blog-implementations/java/play/sample-blog/app/controllers/Categories.java
@@ -4,6 +4,7 @@ import models.Category;
 import play.*;
 import play.mvc.*;
 
+@With(Secure.class)
 @CRUD.For(Category.class)
 public class Categories extends CRUD {
 	
